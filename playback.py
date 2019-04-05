2d60be78e9b7bc757dddf8cff7e6446a48e19123
Magnus Johnson <magnus@magmhj.com>
2019-04-05 17:59:24 -0400

    Added some rudimentary audio support

--- playback.py
+++ playback.py
@@ -1,13 +1,13 @@
 from math import *
 import numpy as np
+from pydub import AudioSegment
+from pydub.playback import play
 
 class PlaybackSystem(object):
-    def __init__(self, t=0, beat_callback=None):
+    def __init__(self, t=0):
         self.t = t
         self.beats_per_minute = 100
         self.current_beat = 0
-
-        self.beat_callback = beat_callback
 
     def quantize_time_to_beat(self, time, round_up=True):
         bps = self.beats_per_minute/60.0
@@ -15,12 +15,19 @@ class PlaybackSystem(object):
         if round_up == False: beat = int(round(time * bps))
         return beat
 
+    def beat_callback(self, beat):
+        if beat % 4 == 0:
+            print("BEAT: "+str(beat))
+            sfx = AudioSegment.from_wav("./sfx/pop.wav")
+            play(sfx)
+        else:
+            sfx = AudioSegment.from_wav("./sfx/pop2.wav")
+            play(sfx)
+
     def on_update(self, dt=0.1):
         self.t += dt
 
         beat = self.quantize_time_to_beat(self.t, False)
         if beat != self.current_beat:
             self.current_beat = beat
-            if self.beat_callback != None: self.beat_callback(self.current_beat)
-
-            if self.current_beat % 4 == 0: print("BEAT: "+str(self.current_beat))
+            self.beat_callback(self.current_beat)
