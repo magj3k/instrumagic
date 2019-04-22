13011e6a8a88ce36e5ac5aab1e1e1d7c24a8683d
Magnus Johnson <magnus@magmhj.com>
2019-04-21 22:00:38 -0400

    Added code to make chord detection more robust

--- pitch_tracker.py
+++ pitch_tracker.py
@@ -43,15 +43,35 @@ class PitchTracker(object):
                     base_note = sorted_common_pitches_and_frequencies[1]
                 if base_note[1] > 7:
 
-                    # extracts supporting note
-                    best_match = None
-                    for pitch_and_freq in common_pitches_and_frequencies:
-                        if pitch_and_freq != base_note and pitch_and_freq[1] >= 6:
-                            if best_match == None or pitch_and_freq[1] > best_match[1] and (best_match[0] == base_note[0]+3 or best_match[0] == base_note[0]+4):
-                                best_match = pitch_and_freq
+                    # checks that base notes are relatively continuous in recorded pitches
+                    base_sufficiently_continuous = False
+                    for i in range(2, len(self.current_pitches)-2):
+                        subset = self.current_pitches[i-2:i+3]
+                        base_count = 0
 
-                    relevant_pitches.append(base_note[0])
-                    if best_match != None: relevant_pitches.append(best_match[0])
+                        min_pitch = 99999
+                        max_pitch = 0
+                        for pitch in subset:
+                            min_pitch = min(min_pitch, pitch)
+                            max_pitch = max(max_pitch, pitch)
+                            if pitch == base_note[0]:
+                                base_count += 1
+                        max_range = abs(min_pitch-max_pitch)
+
+                        if base_count >= 4 and max_range <= 2:
+                            base_sufficiently_continuous = True
+                            break
+
+                    if base_sufficiently_continuous == True:
+                        # extracts supporting note
+                        best_match = None
+                        for pitch_and_freq in common_pitches_and_frequencies:
+                            if pitch_and_freq != base_note and pitch_and_freq[1] >= 6:
+                                if best_match == None or pitch_and_freq[1] > best_match[1] and (best_match[0] == base_note[0]+3 or best_match[0] == base_note[0]+4):
+                                    best_match = pitch_and_freq
+
+                        relevant_pitches.append(base_note[0])
+                        if best_match != None: relevant_pitches.append(best_match[0])
 
         self.current_pitches = []
         return relevant_pitches
@@ -71,7 +91,7 @@ class PitchTracker(object):
                     self.all_frames.extend(sample[0])
 
                 avg_volume = np.mean(np.absolute(self.all_frames))
-                if avg_volume > 0.01: # tracks current pitch if volume is loud enough
+                if avg_volume > 0.045: # tracks current pitch if volume is loud enough
 
                     aubio_samples = np.array(self.all_frames[-1024:], dtype=aubio.float_type)
                     if len(aubio_samples) >= 1024:
